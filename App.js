f9c287e9cf00d2a05984bdd6a4d4b09990a6ae7e
Micah Puzio <52171693+micihs@users.noreply.github.com>
2022-08-14 18:51:12 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { useContext, useState } from "react";
+import { View, Text, TouchableOpacity, Image, Platform, StyleSheet, ScrollView } from "react-native";
+
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Universal React with Expo</Text>
+    </View>
+  );
+}
+
+
+
+
+
+const AuthStack = ({navigation}) => {};
---
+++ Auth/AppStack.js
@@ -0,0 +1,10 @@
+////////
+///
+///This is the file you would construct the rest of your app navigation stacks/tabs. 
+///
+///titling the primary app navigation component AppStack and Exporting it into the Routes file should auto complete the App.
+///allowing prebuilt login/ signup functionality for any moderately scalable applications without much demand for user data.
+///
+///
+///
+///
